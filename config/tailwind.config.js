f63751ae595afe5ce07dc1f89f135f46c5560bbf
shreyas <shreyas314159@gmail.com>
2024-05-03 20:56:34 +0100

    reset the tailwind color and made it uniform

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,33 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
-
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*'
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
       // custom color palette for branding, see https://tailwindcss.com/docs/customizing-colors
       colors: {
-        'primary': '#249044',
-               'primary-hover': {
-                 '700': '#217B38',
-                 '500': '#217B38'
+        primary: "#249044",
+        "primary-hover": {
+          700: "#249044",
+          500: "#249044",
+        },
       },
       keyframes: {
-        flashfade: { "0%, 100%": { opacity: "0" }, "5%, 80%": { opacity: "1" } },
+        flashfade: {
+          "0%, 100%": { opacity: "0" },
+          "5%, 80%": { opacity: "1" },
+        },
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
