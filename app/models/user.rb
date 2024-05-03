a4cf018650910325072fe19ce4a33ba4fb3143e3
shreyas <shreyas314159@gmail.com>
2024-05-03 17:17:02 +0100

    added the two way bidirectional links

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   include Billable
 
   scope :subscribed, -> { where.not(stripe_subscription_id: [nil, '']) }
+  has_many :projects
+  has_many :subscribers
+  has_many :stakeholder_updates, through: :projects
 end
