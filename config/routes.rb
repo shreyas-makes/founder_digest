f76211106c652b11779f6866a831f73abde9d91e
shreyas <shreyas314159@gmail.com>
2024-05-04 12:48:16 +0100

    created a start page too

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'apply', to: 'pages#apply'
+  get 'apply', to: 'pages#start'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
   get 'logout', to: 'pages#logout', as: 'logout'
